e5009d1014e41697482d51aaaeae4be283c8ebdf
takuya kinoshita <kktak02@gmail.com>
2022-12-13 08:17:50 +0900

    init nuxt app

---
+++ pages/hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello world!!</h1>
+</template>
+
+<script>
+  import Tutorial from '../components/Tutorial.vue'
+  export default {
+    name: "Hello",
+    data() {
+      return {}
+    },
+    components: {
+      Tutorial
+    }
+  }
+</script>
